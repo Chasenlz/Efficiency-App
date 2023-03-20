e986b75cca0ba00fd00a1668cf372f20e42301e6
Chase Lazarian <40992932+Chasenlz@users.noreply.github.com>
2023-03-20 18:13:54 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const clickButton = document.getElementById("clickButton");
-const clickCounter = document.getElementById("clickCounter");
-
-let clicks = 0;
-
-clickButton.addEventListener("click", () => {
-  clicks++;
-  clickCounter.textContent = `${clicks} ${clicks === 1 ? "click" : "clicks"}`;
-});
