bc472ce1af82df4b6a7008329ff130c36d195e03
Chase Lazarian <40992932+Chasenlz@users.noreply.github.com>
2023-03-27 07:12:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,10 +12,34 @@ function openTab(evt, tabName) {
   evt.currentTarget.className += " active";
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
 const financialForm = document.getElementById('financial-form');
 financialForm.addEventListener('submit', async (event) => {
-  
+   // Add this line to prevent form from submitting and refreshing the page
+   event.preventDefault();
+
+   // Your existing code to handle form submission
+
 });
+
+// Function to call GPT-3 API with the prompt
+async function callGpt3Api(prompt) {
+    const response = await fetch('https://api.openai.com/v1/engines/davinci-codex/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${your_api_key}`
+        },
+        body: JSON.stringify({
+            prompt: prompt,
+            max_tokens: 200,
+            n: 1,
+            stop: null,
+            temperature: 0.5
+        })
+    });
+
+    const data = await response.json();
+    return data.choices[0].text;
+}
