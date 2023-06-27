756f4358664684b369ec5f503b57dae80ae59b7e
Chase Lazarian <40992932+Chasenlz@users.noreply.github.com>
2023-06-26 23:03:39 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,19 +8,27 @@ expand_btn.addEventListener("click", () => {
 
 const current = window.location.href;
 
-const allLinks = document.querySelectorAll(".sidebar-links a")
+const allLinks = document.querySelectorAll(".sidebar-links a");
 
 allLinks.forEach((elem) => {
-  elem.addEventListener('click', function() {
+  elem.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    
     const hrefLinkClick = elem.href;
 
     allLinks.forEach((link) => {
-      if (link.href == hrefLinkClick){
+      if (link.href === hrefLinkClick) {
         link.classList.add("active");
       } else {
         link.classList.remove('active');
       }
     });
-  })
+    
+    // Optionally, scroll to the clicked section
+    const sectionId = hrefLinkClick.split('#')[1];
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
-
